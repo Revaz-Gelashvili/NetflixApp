62feabc587247ee20fdc127dd35b14b3a67b4516
Revaz Gelashvili Laptop <revazgelashviliofficial@gmail.com>
2026-04-17 03:57:58 +0400

    card realise date and vote size change

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -16,7 +16,7 @@ export default function Card({
       className="group block transform hover:scale-105 transition-transform duration-300"
     >
       <div
-        className="relative w-38 sm:w-50 h-64 rounded-2xl p-2 overflow-hidden shadow-2xl"
+        className="relative w-38 sm:w-50 h-64 rounded-2xl overflow-hidden shadow-2xl"
         style={{
           backgroundImage: `url(${fullImageUrl})`,
           backgroundSize: "cover",
@@ -37,14 +37,14 @@ export default function Card({
           {isFavourite ? "❤️" : "🤍"}
         </button>
 
-        <div className="absolute bottom-0 p-4 text-white w-full">
+        <div className="absolute bottom-0 p-3 text-white w-full">
           <h3 className="font-bold text-lg leading-tight transition-all duration-300 group-hover:-translate-y-4 group-hover:scale-105 origin-left">
             {title}
           </h3>
 
           <div className="mt-2 flex justify-between items-center gap-2 transition-all duration-300 lg:opacity-0 group-hover:opacity-100 group-hover:-translate-y-2">
             <p className="text-xs text-gray-400">{release_date}</p>
-            <span className="bg-yellow-500 text-black text-[10px] font-bold px-1 sm:px-3 py-1 rounded-2xl text-center">
+            <span className="bg-yellow-500 text-black text-[10px] font-bold px-1.5 py-1.5 sm:px-3 sm:py-1 rounded-2xl text-center">
               IMDB: {vote_average.toFixed(1)}
             </span>
           </div>
